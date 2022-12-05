695e9b7a2c48ec0ed091c7af68f80c23c0681c0c
Alexandria <alexsun1337@gmail.com>
2022-12-05 12:28:15 -0800

    Add directory surfing

--- explorer/views.py
+++ explorer/views.py
@@ -2,10 +2,10 @@
 
 from explorer import app
 from flask import abort, request, jsonify
-import os
+from markupsafe import escape
 from pathlib import Path
 
-current_path = Path(app.config["DEFAULT_PATH"])
+default_path = app.config["DEFAULT_PATH"]
 
 
 def _list_dir(path: Path):
@@ -29,10 +29,14 @@ def _open_file(path: Path):
     pass
 
 
-@app.route("/", methods=["GET"])
-def home():
+@app.route("/", defaults={"subpath": None}, methods=["GET"])
+@app.route("/<path:subpath>", methods=["GET"])
+def home(subpath):
     if request.method == "GET":
-        # TODO if path specified in url, add to current path
-        # if valid dir:
-        return jsonify(_list_dir(current_path))
-        # else abort
+        path = Path(default_path + subpath) if subpath else Path(default_path)
+        if path.is_dir():
+            return jsonify(_list_dir(path))
+        # if valid file:
+        #   open file
+        else:
+            abort(404)
