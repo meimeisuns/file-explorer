0118b0f61d0d3d20aa768d0f8963f7ef0b3f1ff6
Alexandria <alexsun1337@gmail.com>
2022-12-05 12:32:15 -0800

    Update directory test

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,3 +6,8 @@ def test_list_directory(client):
     assert response.status_code == 200
     response_dict = json.loads(response.data)
     assert len(response_dict) == 3
+
+    nested_dir_response = client.get("/another_directory")
+    assert nested_dir_response.status_code == 200
+    nested_dir_response_dict = json.loads(nested_dir_response.data)
+    assert len(nested_dir_response_dict) == 1
